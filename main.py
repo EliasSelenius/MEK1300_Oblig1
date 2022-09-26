f8d6b34dd954bb9b1aa4cdf291941aaab4670207
Elias Sebastian Selenius Haneborg <ESSH@LIVE.NO>
2022-09-26 15:27:27 +0200

    fixed some prints and getNumberInput()

--- main.py
+++ main.py
@@ -1,5 +1,16 @@
 from random import randrange
 
+def getNumberInput():
+    inp = input("Enter a number between 1 and 1000: ")
+    if not inp.isnumeric():
+        print("Invalid input. Try again.")
+        return getNumberInput()
+    guess = int(inp)
+    if guess < 1 or guess > 1000:
+        print("Your guess is out of range. Try again")
+        return getNumberInput()
+    return guess
+
 
 def play():
     randomNum = randrange(1, 1000)
@@ -7,9 +18,9 @@ def play():
     attempts = 0
     while True:    
         attempts += 1
-        guess = int(input("Enter a number between 1 and 1000: "))
+        guess = getNumberInput()
         if guess == randomNum:
-            print("You guessed the correct number, congratz.")
+            print("Congratulation! You guessed the number in %d attempt(s)" % (5 - attempts))
             break
         elif guess < randomNum:
             print("Your guess is too low.")
@@ -17,11 +28,12 @@ def play():
             print("Your guess is too high.")
 
         if attempts == 5:
-            print("Sorry!! you are out of guesses. The answer is %d" % (randomNum))
+            print("Sorry!! You did not manage to guess the number. You have reached the guessing limit. The answer is %d" % (randomNum))
             break
         else:
             print("You have %d attempt(s) left." % (5 - attempts))
 
+print("Welcome to the number guessing game!")
 play()
 
 while True:    
@@ -29,7 +41,7 @@ while True:
     if inp == "YES":
         play()
     elif inp == "NO":
-        print("Bye Bye.")
+        print("Thank you for playing.")
         break
     else:
-        print("%s is not a valid option. Type 'YES' or 'NO'." % (inp))
+        print("'%s' is not a valid option. Type 'YES' or 'NO'." % (inp))
